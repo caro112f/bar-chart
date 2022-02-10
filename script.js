f4c2dec342399697f723a9a3659acc1790b88b6c
Caroline Nielsen <caro112f@stud.kea.dk>
2022-02-10 15:59:12 +0100

    more comments

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ function loop() {
     //^unShift adds the randomized number from queueSize in the array
     counter++;
     //^counter++ adds new queueSize so the array grows
+
+    //in general: loops the array while telling it to increase the array size as long as its under 40, making new bars and implementing random numbers each time
   } else {
     array.length = 39;
     //^defines the array length to be 39
@@ -27,6 +29,8 @@ function loop() {
     //^unShift adds the randomized number from queueSize in the array
     createNewBar();
     //^loads function that creates new bar each time the array loops
+
+    //in general: continues to loop array without counter++ to make it grow, still making new bars each time and implementing randomized numbers
   }
   console.log(array);
   init();
